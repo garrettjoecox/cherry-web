997f35e5799794c22f38cd44d2679937deec8918
Garrett Cox <garrettjcox@gmail.com>
2021-09-11 21:02:30 -0500

    Add placeholder terms & privacy policy pages

---
+++ pages/privacy.tsx
@@ -0,0 +1,35 @@
+import type { NextPage } from "next";
+import {
+  Heading,
+  Text,
+  Box,
+  Container,
+} from "@chakra-ui/react";
+import React from "react";
+
+const Privacy: NextPage = () => {
+    return (
+      <Container minH="100vh" maxW="container.lg" py="4" display="flex">
+        <Box
+        flex="1"
+          bgColor="white"
+          borderColor="red.400"
+          borderWidth="6px"
+          py="10"
+          borderRadius="md"
+        >
+          <Box mx="5">  
+            <Heading fontSize="4xl">
+              Privacy Policy
+            </Heading>
+            <Text>
+              TODO....
+            </Text>
+          </Box>
+        </Box>
+      </Container>
+    );
+  };
+  
+  export default Privacy;
+  
